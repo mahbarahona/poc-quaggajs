9d22e570c0d8a0be486a563b29f4a9c9adb7d289
Manuel Hernandez <mahbarahona@gmail.com>
2022-06-23 13:32:06 -0400

    adjust

--- js/live_w_locator.js
+++ js/live_w_locator.js
@@ -247,10 +247,12 @@ $(function() {
             App.lastResult = code;
             var $node = null, canvas = Quagga.canvas.dom.image;
 
-            $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
-            $node.find("img").attr("src", canvas.toDataURL());
-            $node.find("h4.code").html(code);
-            $("#result_strip ul.thumbnails").prepend($node);
+            $node.find("h4.code").html('tag:'+code);
+            
+            // $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
+            // $node.find("img").attr("src", canvas.toDataURL());
+            // $("#result_strip ul.thumbnails").prepend($node);
+
         }
     });
 });
